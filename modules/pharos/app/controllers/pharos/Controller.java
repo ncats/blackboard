eda29f2ad8989f1de10d3cd8cc19f6f853b28b97
trung <caodac@gmail.com>
2018-05-11 00:06:33 -0400

    reorg each ks controller into its own unique package so that reverse routing works correctly

--- modules/pharos/app/controllers/pharos/Controller.java
+++ modules/pharos/app/controllers/pharos/Controller.java
@@ -1,4 +1,4 @@
-package controllers;
+package controllers.pharos;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -8,13 +8,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Singleton
-public class PharosController extends Controller {
+public class Controller extends play.mvc.Controller {
     @Inject
-    public PharosController (Configuration config) {
+    public Controller (Configuration config) {
     }
 
     public Result index () {
         return ok ("Information about Pharos knowledge source goes here!");
     }
 }
-
--- modules/umls/app/controllers/umls/Controller.java
+++ modules/umls/app/controllers/umls/Controller.java
@@ -1,4 +1,6 @@
-package controllers;
+package controllers.umls;
+
+import java.util.*;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -11,20 +13,22 @@ import play.libs.Json;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 import blackboard.umls.UMLSKSource;
 
 @Singleton
-public class UMLSController extends Controller {
+public class Controller extends play.mvc.Controller {
     final UMLSKSource ks;
     
     @Inject
-    public UMLSController (Configuration config, UMLSKSource ks) {
+    public Controller (Configuration config, UMLSKSource ks) {
         this.ks = ks;
     }
 
     public Result index () {
-        return ok ("This is a basic implementation of the UMLS knowledge source!");
+        return ok
+            ("This is a basic implementation of the UMLS knowledge source!");
     }
 
     public Result ticket () {
@@ -36,10 +40,44 @@ public class UMLSController extends Controller {
         }
     }
 
-    public Result search (String q) {
+    static ObjectNode instrument (JsonNode node) {
+        Map<String, JsonNode> fields = new HashMap<>();
+        for (Iterator<Map.Entry<String, JsonNode>> it =
+                 node.fields(); it.hasNext();) {
+            Map.Entry<String, JsonNode> me = it.next();
+            fields.put(me.getKey(), me.getValue());
+        }
+        ObjectNode n = Json.newObject();
+        n.setAll(fields);
+        
+        String cui = n.get("ui").asText();
+        n.put("_cui", controllers.umls.routes.Controller.cui(cui).url());
+        n.put("_atoms", routes.Controller.atoms(cui).url());
+        n.put("_definitions", routes.Controller.definitions(cui).url());
+        n.put("_relations", routes.Controller.relations(cui).url());
+        
+        return n;
+    }
+
+    public Result search (String q, Integer top, Integer skip) {
         try {
-            WSResponse res = ks.search(q).get().toCompletableFuture().get();
-            return status (res.getStatus(), res.asJson());
+            WSResponse res = ks.search(q)
+                .setQueryParameter("pageSize", top.toString())
+                .setQueryParameter("pageNumber", String.valueOf(skip/top+1))
+                .get().toCompletableFuture().get();
+            JsonNode json = res.asJson();
+            if (json.hasNonNull("result")) {
+                JsonNode results = json.get("result").get("results");
+                ArrayNode ary = Json.newArray();
+                for (int i = 0; i < results.size(); ++i) {
+                    JsonNode r = results.get(i);
+                    ary.add(instrument (r));
+                }
+                json = ary;
+            }
+            else
+                json = Json.newArray();
+            return status (res.getStatus(), json);
         }
         catch (Exception ex) {
             return internalServerError (ex.getMessage());
